ea37830df6fd5342c839992bfe2e9a28c578a797
scalamari <santi.calamari@gmail.com>
2019-08-13 12:16:25 -0300

    informate

--- resources/views/web/layouts/asesorate.blade.php
+++ resources/views/web/layouts/asesorate.blade.php
@@ -5,62 +5,109 @@
     <body>
         <div class="col-12 mujeres-libres">
             <h1 class="titulo-seccion bs-docs-featurette-title"> Asesorate </h1>
+            <p class="slogan">Permite contactarse con el 144, 911 y el 0800 777 5000 y otros organismos especializados.</p>
         </div>
         <br>
         <div class="container">
-            <div class="row text-center">
+
+            <div class='row'>                
                 <div class="col-md-12">
-                    <div class="row "style="padding-top:20px;">
-                        <div class="col-md-4 col-sm-4 col-xs-6">
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h4>Donde asesorarte</h4> 
-                                </div>
-                                <div class="panel-body">
-                                    <p>
-                                        Area Mujer y Diversidad Sexual                                            
-                                    </p>
-                                    <p>
-                                        Defensoria del Pueblo                                            
-                                    </p>
-                                </div>
-                                <div class="panel-footer">
-                                </div>
+                    <h4 class="card-title">¿Dónde pedir ayuda?</h4>
+                </div>
+                <div class="col-md-6">
+                    <div class="card asesorate-card">
+                        <div class="card-body">
+                            <h4 class="card-title">Área Mujer y Diversidad Sexual</h4>
+                            <div class="asesorate-body">
+                                <h5 class="card-text">25 de Mayo 2882, Santa Fe</h5>
+                                <h5 class="card-text">Tel: 4571525/4571666(Lunes a viernes de 8 a 20 hs.)</h5>
+                                <h5 class="card-text">Línea gratuita 144 - 0800 777 5000 (Todos los días, 24 hs.)</h5>
                             </div>
+                            <a target="_blank" href="https://www.santafeciudad.gov.ar/servicios/mujer-y-diversidad-sexual/" class="card-link">Web</a>
+                            <a target="_blank" href="https://www.google.com/maps/place/25+de+Mayo+2884,+S3000FUA+Santa+Fe/@-31.6425636,-60.7067479,17z/data=!4m13!1m7!3m6!1s0x95b5a9bb82ffdf23:0xd8896013bbb98f9e!2s25+de+Mayo+2884,+S3000FUA+Santa+Fe!3b1!8m2!3d-31.6425636!4d-60.7045592!3m4!1s0x95b5a9bb82ffdf23:0xd8896013bbb98f9e!8m2!3d-31.6425636!4d-60.7045592" class="card-link">Cómo llegar</a>
                         </div>
-                        <div class="col-md-4 col-sm-4 col-xs-6">
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h4>Donde denunciar</h4> 
-                                </div>
-                                <div class="panel-body">
-                                    <p>
-                                        Centros territoriales de denuncia
-                                    </p>
-                                </div>
-                                <div class="panel-footer">
-                                </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card asesorate-card">
+                        <div class="card-body">
+                            <h4 class="card-title">Defensoría del Pueblo</h4>
+                            <div class="asesorate-body">
+                                <h5 class="card-text">Eva Peron 2726, Santa Fe</h5>
+                                <h5 class="card-text">Tel: 4573910 / 4572694</h5>
                             </div>
+                            <a target="_blank" href="https://www.defensoriasantafe.gob.ar/" class="card-link">Web</a>
+                            <a target="_blank" href="https://www.google.com/maps/place/Defensor%C3%ADa+del+Pueblo/@-31.644145,-60.707702,15z/data=!4m2!3m1!1s0x0:0x41c648d5be3abdc8?sa=X&ved=2ahUKEwjkldepgoDkAhV0HbkGHfTuCaQQ_BIwCnoECA4QCA" class="card-link">Cómo llegar</a>
                         </div>
-                        <div class="col-md-4 col-sm-4 col-xs-6">
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h4>Lineas gratuitas</h4> 
-                                </div>
-                                <div class="panel-body">
-                                    <p>Las 24 horas, los 365 dias</p>
-                                    <p>144</p>
-                                    <p>0800-777-5000</p>
-                                    <p>911</p>
-                                </div>
-                                <div class="panel-footer">
-                                </div>
+                    </div>
+                </div>                
+            </div>
+            <br>
+            <div class='row'>
+                <div class="col-md-12">
+                    <h4 class="card-title">¿Dónde denunciar?</h4>
+                </div>
+                <div class="col-md-4">
+                    <div class="card asesorate-card">
+                        <div class="card-body">
+                            <h4 class="card-title">Centros Territoriales de Denuncias</h4>
+                            <div class="asesorate-body">
+                                <h5 class="card-text">Centro: Las Heras 2882</h5>
+                                <h5 class="card-text">Tel: 4815578 / 4559785</h5>
                             </div>
-                        </div>                            
+                            <a target="_blank" href="https://www.defensoriasantafe.gob.ar/" class="card-link">Web</a>
+                            <a target="_blank" href="https://www.google.com/maps/place/Las+Heras+2882,+S3000GDE+Santa+Fe/@-31.6435404,-60.7016614,17z/data=!3m1!4b1!4m5!3m4!1s0x95b5a9bd48ff926f:0x44111669ec1ded54!8m2!3d-31.6435404!4d-60.6994674" class="card-link">Cómo llegar</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card asesorate-card">
+                        <div class="card-body">
+                            <h4 class="card-title">Centros Territoriales de Denuncias</h4>
+                            <div class="asesorate-body">
+                                <h5 class="card-text">Norte: Aristóbulo del Valle 7401</h5>
+                                <h5 class="card-text">Tel.:4833446 Horario de atención: 8 a 20 hs</h5>
+                            </div>
+                            <a target="_blank" href="https://www.santafe.gov.ar/index.php/web/content/view/full/198373/(subtema)/93821" class="card-link">Web</a>
+                            <a target="_blank" href="https://www.google.com/maps/place/Av.+Arist%C3%B3bulo+del+Valle+7401,+Santa+Fe/@-31.6002296,-60.6941018,17z/data=!3m1!4b1!4m5!3m4!1s0x95b50744a6f1c3c9:0x1b7f937982c8962a!8m2!3d-31.6002296!4d-60.6919078" class="card-link">Cómo llegar</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card asesorate-card">
+                        <div class="card-body">
+                            <h4 class="card-title">Centro de Orientación a la Víctima de Violencia Familiar y Sexual</h4>
+                            <div class="asesorate-body">
+                                <h5 class="card-text">Lisandro de la Torre 2665.</h5>
+                                <h5 class="card-text">Tel: 4619923</h5>
+                            </div>
+                            <a target="_blank" href="https://www.santafeciudad.gov.ar/tag/violencia/" class="card-link">Web</a>
+                            <a target="_blank" href="https://www.google.com/maps/place/Lisandro+de+la+Torre+2665,+S3000+Santa+Fe/data=!4m2!3m1!1s0x95b5a9a5e3839bdb:0x1f79704fbc97d195?sa=X&ved=2ahUKEwj65L7yhYDkAhWDC9QKHQzNAVEQ8gEwAHoECAsQAQ" class="card-link">Cómo llegar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class='row'>
+                <div class="col-md-12">
+                    <h4 class="card-title">¿Dónde solicitar las medidas de ley de Protección Integral de las mujeres?</h4>
+                </div>
+                <div class="col-md-12">
+                    <div class="card asesorate-card">
+                        <div class="card-body">
+                            <h4 class="card-title">Unidad de Información y Atención de Víctimas y Denunciantes</h4>
+                            <div class="asesorate-body">
+                                <h5 class="card-text">Urquiza 2463</h5>
+                                <h5 class="card-text">Lunes a viernes de 7.15 a 12.45 y de 14.15 a 19.45 hs.</h5>
+                            </div>
+                            <a target="_blank" href="https://www.mpf.gob.ar/" class="card-link">Web</a>
+                            <a target="_blank" href="https://www.google.com/maps/place/Urquiza+2463,+S3000FFJ+Santa+Fe/@-31.6456463,-60.7153874,17z/data=!3m1!4b1!4m5!3m4!1s0x95b5a9b1b1bc14d7:0x4a5a7e1a53336c12!8m2!3d-31.6456463!4d-60.7131934" class="card-link">Cómo llegar</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </body>
+    <br> <br> <br> <br>
 </html>
 
--- resources/views/web/layouts/informate.blade.php
+++ resources/views/web/layouts/informate.blade.php
@@ -16,16 +16,28 @@
                     </a>
                     <div id="collapseOne" class="collapse" data-parent="#accordion">
                         <div class="card-body">
-                            <h4>FÍSICA</h4>
-                            <p>Es la que se emplea contra tu cuerpo y produce dolor, daño o riesgo de producirlo.</p>
-                            <h4>PSICOLÓGICA</h4>
-                            <p>Es aquella que causa daño emocional y disminución de la autoestima, perjudica y perturba el pleno desarrollo personal, o busca degradar o controlar tus acciones.</p>
-                            <h4>SEXUAL</h4>
-                            <p>Cualquier acción que implique la vulneración en todas sus formas, con o sin acceso genital, de tu derecho a decidir voluntariamente tu vida sexual o reproductiva.</p>
-                            <h4>ECONÓMICA Y PATRIARCAL</h4>
-                            <p>Es la que se dirige a ocasionar un menoscabo en tus recursos económicos o patrimoniales.</p>
-                            <h4>SIMBÓLICA</h4>
-                            <p>La que a través de patrones estereotipados, mensajes, valores, íconos o signos transmita y reproduzca dominación, desigualdad y discriminación en las relaciones sociales.</p>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">
+                                    <h4>FÍSICA</h4>
+                                    <p>Es la que se emplea contra tu cuerpo y produce dolor, daño o riesgo de producirlo.</p>
+                                </li>
+                                <li class="list-group-item">
+                                    <h4>PSICOLÓGICA</h4>
+                                    <p>Es aquella que causa daño emocional y disminución de la autoestima, perjudica y perturba el pleno desarrollo personal, o busca degradar o controlar tus acciones.</p>
+                                </li>
+                                <li class="list-group-item">
+                                    <h4>SEXUAL</h4>
+                                    <p>Cualquier acción que implique la vulneración en todas sus formas, con o sin acceso genital, de tu derecho a decidir voluntariamente tu vida sexual o reproductiva.</p>
+                                </li>
+                                <li class="list-group-item">
+                                    <h4>ECONÓMICA Y PATRIARCAL</h4>
+                                    <p>Es la que se dirige a ocasionar un menoscabo en tus recursos económicos o patrimoniales.</p>
+                                </li>
+                                <li class="list-group-item">
+                                    <h4>SIMBÓLICA</h4>
+                                    <p>La que a través de patrones estereotipados, mensajes, valores, íconos o signos transmita y reproduzca dominación, desigualdad y discriminación en las relaciones sociales.</p>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
@@ -36,16 +48,28 @@
                     </a>
                     <div id="collapseTwo" class="collapse" data-parent="#accordion">
                         <div class="card-body">
-                            <h4>DOMÉSTICA</h4>
-                            <p>La ejercida por un integrante del grupo familiar.</p>
-                            <h4>INSTITUCIONAL</h4>
-                            <p>La realizada por funcionarios, profesionales y personal de cualquier institución pública que retarde, obstaculice o impida el acceso a las políticas públicas y el ejercicio de tus derechos.</p>
-                            <h4>OBSTÉTRICA, REPRODUCTIVA Y NO REPRODUCTIVA</h4>
-                            <p>La que te impide decidir libremente el número de embarazos o el intervalo entre los nacimientos y la que ejerce el personal de salud sobre tu cuerpo y los procesos reproductivos.</p>
-                            <h4>LABORAL</h4>
-                            <p>La que en el ámbito del trabajo, genera diferencias basadas en el género como ser la remuneración desigual ante igual tarea o la imposibilidad de movilidad laboral.</p>
-                            <h4>MEDIÁTICA</h4>
-                            <p>La que se ejercen medios de comunicación y publicidades, con mensajes y valores que reproducen la dominación, la desigualdad y la discriminación en las relaciones sociales.</p>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">
+                                    <h4>DOMÉSTICA</h4>
+                                    <p>La ejercida por un integrante del grupo familiar.</p>
+                                </li>
+                                <li class="list-group-item">
+                                    <h4>INSTITUCIONAL</h4>
+                                    <p>La realizada por funcionarios, profesionales y personal de cualquier institución pública que retarde, obstaculice o impida el acceso a las políticas públicas y el ejercicio de tus derechos.</p>
+                                </li>
+                                <li class="list-group-item">
+                                    <h4>OBSTÉTRICA, REPRODUCTIVA Y NO REPRODUCTIVA</h4>
+                                    <p>La que te impide decidir libremente el número de embarazos o el intervalo entre los nacimientos y la que ejerce el personal de salud sobre tu cuerpo y los procesos reproductivos.</p>
+                                </li>
+                                <li class="list-group-item">
+                                    <h4>LABORAL</h4>
+                                    <p>La que en el ámbito del trabajo, genera diferencias basadas en el género como ser la remuneración desigual ante igual tarea o la imposibilidad de movilidad laboral.</p>
+                                </li>
+                                <li class="list-group-item">
+                                    <h4>MEDIÁTICA</h4>
+                                    <p>La que se ejercen medios de comunicación y publicidades, con mensajes y valores que reproducen la dominación, la desigualdad y la discriminación en las relaciones sociales.</p>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </div>
@@ -56,15 +80,17 @@
                     </a>
                     <div id="collapseThree" class="collapse" data-parent="#accordion">
                         <div class="card-body">
-                            <p>La gratuidad de las actuaciones judiciales y del patrocinio jurídico preferentemente especializado.</p>
-                            <p>Protección de tu intimidad, garantizando la confidencialidad de las actuaciones.</p>
-                            <p>Recibir un trato humanizado, evitando la revictimización.</p>
-                            <p>La amplitud probatoria para acreditar los hechos denunciados.</p>
-                            <p>Oponerte a la realización de inspecciones sobre tu cuerpo por fuera de la orden judicial.</p>
-                            <p>Contar con mecanismos eficientes para denunciar a los funcionarios por el incumplimiento.</p>
-                            <p>Solicitar asistencia integral y acompañamiento de un equipo especializado.</p>
-                            <p>Prohibición de mediaciones y conciliaciones con la persona que ejerce violencia.</p>
-                            <p>Solicitar medidas preventivas urgentes: exclusión y/o restricción perimetral, botón de alerta, restitución de bienes y personas, régimen de alimentos y de cuidados personales.</p>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">La gratuidad de las actuaciones judiciales y del patrocinio jurídico preferentemente especializado.</li>
+                                <li class="list-group-item">Protección de tu intimidad, garantizando la confidencialidad de las actuaciones.</li>
+                                <li class="list-group-item">Recibir un trato humanizado, evitando la revictimización.</li>
+                                <li class="list-group-item">La amplitud probatoria para acreditar los hechos denunciados.</li>
+                                <li class="list-group-item">Oponerte a la realización de inspecciones sobre tu cuerpo por fuera de la orden judicial.</li>
+                                <li class="list-group-item">Contar con mecanismos eficientes para denunciar a los funcionarios por el incumplimiento.</li>
+                                <li class="list-group-item">Solicitar asistencia integral y acompañamiento de un equipo especializado.</li>
+                                <li class="list-group-item">Prohibición de mediaciones y conciliaciones con la persona que ejerce violencia.</li>
+                                <li class="list-group-item">Solicitar medidas preventivas urgentes: exclusión y/o restricción perimetral, botón de alerta, restitución de bienes y personas, régimen de alimentos y de cuidados personales.</li>                                
+                            </ul>
                         </div>
                     </div>
                 </div>
